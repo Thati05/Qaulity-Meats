5eab7a219770323c128319d7668fad99bbc08393
Thati05 <m.seithati01@gmail.com>
2024-12-12 15:03:25 +0200

    Navigation/Routing

--- components/Menubar.jsx
+++ components/Menubar.jsx
@@ -3,12 +3,15 @@ import React, { useState } from "react";
 import Navlinks from "@/constants/index";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from '@/public/assets/images/Logo.png'
+
 
 export default function Menubar() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsOpen(!isOpen);
+    setIsOpen(!isOpen); //true
   };
 
   return (
@@ -24,18 +27,30 @@ export default function Menubar() {
 
       {/* Sidebar Menu */}
       <div
-        className={`fixed top-0 left-0 h-full bg-gray-800 text-white transform ${
+        className={`fixed top-0 left-0 h-full bg-white shadow-md text-black transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-300 z-50`}
       >
-        <ul className="flex flex-col p-4 space-y-4">
+        <ul className="flex flex-col p-5 pr-12 space-y-5">
+           <Image width={80} src={Logo} alt=" Quality Meats logo" />
           {Navlinks.map((link, index) => (
-            <li key={index} className="hover:bg-gray-700 p-2 rounded-lg">
+            <li key={index} className=" relative w-fit group">
               <Link onClick={() => setIsOpen(false)} href={link.href}>
                {link.name}
+               <span className="absolute bottom-0 left-0 w-0 h-[3px] bg-crimsonRed transition-all duration-300 ease-in-out group-hover:w-full"></span>
               </Link>
+              
             </li>
+            
           ))}
+          <li className=" relative w-fit group">
+
+          <Link onClick={() => setIsOpen} href='/contact' >
+              Contact
+              </Link>
+              <span className=" absolute bottom-0 left-0 w-0 h-[3px] bg-crimsonRed transition-all duration-300 ease-in-out group-hover:w-full"></span>
+          </li>
+         
         </ul>
       </div>
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,47 @@
-import Image from 'next/image'
-import React from 'react'
-import Logo from '@/public/assets/images/Logo.png'
-import Navlinks from '@/constants/index'
-import Link from 'next/link'
-import Button from './Button'
-import Menubar from './Menubar'
+"use client";
 
-
-
-
+import Image from "next/image";
+import React, { useState } from "react";
+import Logo from "@/public/assets/images/Logo.png";
+import Navlinks from "@/constants/index";
+import Link from "next/link";
+import Button from "./Button";
+import Menubar from "./Menubar";
 
 export default function Navbar() {
-  return (
-    <nav className=' flex  justify-between items-center px-5 py-8 font-[family-name:var(--font-mont-sans)]'>
-      <Image width={120} alt='Quality Meats Logo' src={Logo}/>
-      <ul className=' md:flex md:gap-10 lg:gap-20  text-lg hidden'>
-        {Navlinks.map((links, index) => (
-           <li key={index}>
-            <Link href={links.href}>
-            {links.name}
-            </Link>
+  const [selected, setSelected] = useState<number | null>(null);
 
-        </li>
+  const handleNavClick = (index: number): void => {
+    setSelected(index);
+  };
+
+  return (
+    <nav className="flex justify-between items-center px-5 py-8 font-[family-name:var(--font-mont-sans)]">
+      <Image width={120} alt="Quality Meats Logo" src={Logo} />
+      <ul className="md:flex md:gap-10 lg:gap-20 text-lg hidden">
+        {Navlinks.map((link, index) => (
+          <li key={index} className="relative group">
+            <Link
+              href={link.href}
+              className={`after:content-[''] after:block after:w-full after:h-[4px] after:bg-[#A41A21] after:transition-transform after:duration-300 ${
+                selected === index
+                  ? "after:scale-x-100"
+                  : "after:scale-x-0 hover:after:scale-x-100"
+              }`}
+              onClick={() => handleNavClick(index)}
+            >
+              {link.name}
+            </Link>
+          </li>
         ))}
-       
       </ul>
 
-      <Button label='Contact' href='/contact' className=' bg-[#A41A21] md:flex hidden text-white' />
-      <Menubar/>
-     
-     
+      <Button
+        label="Contact"
+        href="/contact"
+        className="bg-[#A41A21] md:flex hidden text-white"
+      />
+      <Menubar />
     </nav>
-  )
+  );
 }
