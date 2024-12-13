42c85d5799264b1e8779aba5f26b444a3543207c
Thati05 <m.seithati01@gmail.com>
2024-12-13 18:05:51 +0200

    Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Hero from "@/sections/Hero";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-mont-sans)]">
-     Home page
+    <div className=" items-center justify-items-center font-[family-name:var(--font-mont-sans)]">
+   <Hero/>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        crimsonRed:'#A41A21'
+        crimsonRed: '#A41A21',
+      },
+      animation: {
+        'move-left': 'move-left 30s linear infinite',
+        'move-right': 'move-right 30s linear infinite',
+      },
+      keyframes: {
+        'move-left': {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+        'move-right': {
+          '0%': { transform: 'translateX(-50%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
       },
     },
   },
